cd97b8f161eb184cb3b6a694d571cb6afb8be49c
gorjanz <gorjanz@gmail.com>
2014-03-09 18:00:04 +0100

    - Exceptions added for the situations where the text cannot be recognized

---
+++ VodiSmetka/src/com/vodismetka/exceptions/UnrecognizableDateException.java
@@ -0,0 +1,14 @@
+package com.vodismetka.exceptions;
+
+public class UnrecognizableDateException extends Exception {
+
+	public UnrecognizableDateException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UnrecognizableDateException(String detailMessage) {
+		super(detailMessage);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ VodiSmetka/src/com/vodismetka/exceptions/UnrecognizablePriceException.java
@@ -0,0 +1,17 @@
+package com.vodismetka.exceptions;
+
+public class UnrecognizablePriceException extends Exception{
+
+	public UnrecognizablePriceException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UnrecognizablePriceException(String detailMessage) {
+		super(detailMessage);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
